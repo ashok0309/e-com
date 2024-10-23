3896560883b32bfc84da4dd1b0b3166eaebc23b5
ashok0309 <sadhuashok03@gmail.com>
2024-10-23 13:20:12 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/https://github.com/ashok0309/e-commerce.git/",
+  base:"/e-commerce/",
   plugins: [react()],
 })
